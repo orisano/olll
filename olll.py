287e9cb9eac5c27f61ddc9b02e4c9db23df88504
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-21 22:48:21 +0900

    feat: add Vector class

--- olll.py
+++ olll.py
@@ -1,69 +1,104 @@
-import operator
+from fractions import Fraction
 from typing import Iterator, List, Sequence
 
-Vector = Sequence[float]
 
-def _dot(a: Vector, b: Vector) -> float:
-    """
-    >>> _dot((1.0, 2.0, 3.0), (4.0, 5.0, 6.0))
-    32.0
-    """
-    assert len(a) == len(b)
-    return sum(map(lambda x: x[0] * x[1], zip(a, b)))
+class Vector(list):
+    def __init__(self, x):
+        super().__init__(map(Fraction, x))
+
+    def sdot(self):
+        return self.dot(self)
+
+    def dot(self, rhs: "Vector") -> Fraction:
+        """
+        >>> Vector([1, 2, 3]).dot([4, 5, 6])
+        Fraction(32, 1)
+        """
+        rhs = Vector(rhs)
+        assert len(self) == len(rhs)
+        return sum(map(lambda x: x[0] * x[1], zip(self, rhs)))
+
+    def proj_coff(self, rhs: "Vector") -> Fraction:
+        """
+        >>> Vector([1, 1, 1]).proj_coff([-1, 0, 2])
+        Fraction(1, 3)
+        """
+        rhs = Vector(rhs)
+        assert len(self) == len(rhs)
+        return self.dot(rhs) / self.sdot()
+
+    def proj(self, rhs: "Vector") -> "Vector":
+        """
+        >>> Vector([1, 1, 1]).proj([-1, 0, 2])
+        [Fraction(1, 3), Fraction(1, 3), Fraction(1, 3)]
+        """
+        rhs = Vector(rhs)
+        assert len(self) == len(rhs)
+        return self.proj_coff(rhs) * self
+
+    def __sub__(self, rhs: "Vector") -> "Vector":
+        """
+        >>> Vector([1, 2, 3]) - [6, 5, 4]
+        [Fraction(-5, 1), Fraction(-3, 1), Fraction(-1, 1)]
+        """
+        rhs = Vector(rhs)
+        assert len(self) == len(rhs)
+        return Vector(x - y for x, y in zip(self, rhs))
+
+    def __mul__(self, rhs: Fraction) -> "Vector":
+        """
+        >>> Vector(["3/2", "4/5", "1/4"]) * 2
+        [Fraction(3, 1), Fraction(8, 5), Fraction(1, 2)]
+        """
+        return Vector(x * rhs for x in self)
+
+    def __rmul__(self, lhs: Fraction) -> "Vector":
+        """
+        >>> 2 * Vector(["3/2", "4/5", "1/4"])
+        [Fraction(3, 1), Fraction(8, 5), Fraction(1, 2)]
+        """
+        return Vector(x * lhs for x in self)
 
 
-def _sub(a: Vector, b: Vector) -> Vector:
+def gramschmidt(v: Sequence[Vector]) -> Sequence[Vector]:
     """
-    >>> _sub((1.0, 2.0, 3.0), (4.0, 5.0, 6.0))
-    (-3.0, -3.0, -3.0)
-    """
-    assert len(a) == len(b)
-    return tuple(x - y for x, y in zip(a, b))
-
-
-def _mul(a: Vector, b: float) -> Vector:
-    """
-    >>> _mul((1.0, 2.0, 3.0), 4.0)
-    (4.0, 8.0, 12.0)
-    """
-    return tuple(x * b for x in a)
-
-
-def gramschmidt(v: Sequence[Vector]) -> Iterator[Vector]:
-    """
-    >>> list(gramschmidt([[1, 2], [3, 4]]))
-    [(1, 2), (0.8, -0.4)]
+    >>> gramschmidt([[1, 3], [2, 2]])
+    [[Fraction(1, 1), Fraction(3, 1)]]
     """
     u: List[Vector] = []
-    cache_dot: List[float] = []
     for vi in v:
-        ui = tuple(vi)
-        for uj, dot in zip(u, cache_dot):
-            ui = _sub(ui, _mul(uj, _dot(uj, vi) / dot))
-        yield ui
-        u.append(ui)
-        cache_dot.append(_dot(ui, ui))
+        ui = Vector(vi)
+        for uj in u:
+            ui = ui - ui.proj(uj)
+        if any(ui):
+            u.append(ui)
+    return u
 
 
-def reduction(basis: Sequence[Sequence[int]], delta: float):
+def reduction(basis: Sequence[Sequence[int]], delta: float) -> Sequence[Vector]:
+    """
+    >>> reduction([[4, 1, 2], [4, 7, 2], [3, 1, 7]], 0.9)
+    [[4, 1, 2], [-1, 0, 5], [0, 6, 0]]
+    """
     n = len(basis)
-    ortho = list(gramschmidt(basis))
+    basis = list(map(Vector, basis))
+    ortho = gramschmidt(basis)
 
-    def mu(i: int, j: int) -> float:
-        return _dot(basis[i], ortho[j]) / _dot(ortho[j], ortho[j])
+    def mu(i: int, j: int) -> Fraction:
+        return abs(ortho[j].proj_coff(basis[i]))
 
     k = 1
-    while k <= n:
+    while k < n:
         for j in range(k - 1, -1, -1):
             mu_kj = mu(k, j)
             if mu_kj > 0.5:
-                basis[k] = _sub(basis[k], _mul(basis[j], mu_kj))
-                ortho = list(gramschmidt(basis))
-        if _dot(ortho[k], ortho[k]) >= (delta - mu(k, k - 1)**2) * _dot(ortho[k - 1], ortho[k - 1]):
+                basis[k] = basis[k] - basis[j] * mu_kj
+                ortho = gramschmidt(basis)
+        if ortho[k].sdot() >= (delta - mu(k, k - 1)**2) * ortho[k - 1].sdot():
             k += 1
         else:
             basis[k], basis[k - 1] = basis[k - 1], basis[k]
-            ortho = list(gramschmidt(basis))
+            ortho = gramschmidt(basis)
             k = max(k - 1, 1)
 
     return basis
