fb7b3f53daf7feb19a089e3e472a3a849dac9dcd
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-23 20:13:26 +0900

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="olll",
+    version="1.0.0",
+    author="orisano",
+    author_email="owan.orisano@gmail.com",
+    description="A Python3 implementation of the LLL",
+    license="MIT",
+    url="https://github.com/orisano/olll",
+    py_modules=["olll"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
