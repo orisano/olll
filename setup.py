3892ad95c8ee13760337e004f30b8db1f81ffc12
Nao Yonashiro <owan.orisano@gmail.com>
2020-04-04 22:23:31 +0900

    feat: change supported versions
    drop 3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="olll",
-    version="1.0.1",
+    version="1.0.2",
     author="orisano",
     author_email="owan.orisano@gmail.com",
     description="A Python3 implementation of the LLL",
@@ -13,8 +13,9 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
 )
