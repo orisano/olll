ca4d3a15b46da3299e11e31174425fec3b154511
Nao Yonashiro <owan.orisano@gmail.com>
2021-04-29 16:53:49 +0900

    refactor: use setup.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name="olll",
-    version="1.0.2",
-    author="Nao Yonashiro",
-    author_email="owan.orisano@gmail.com",
-    description="A Python3 implementation of the LLL",
-    license="MIT",
-    url="https://github.com/orisano/olll",
-    py_modules=["olll"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-)
+setup()
